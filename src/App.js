a1f06d426cee790320225a084dfa7d2db4f539bb
oleh_matsevych <olegmacevich@ukr.net>
2021-04-02 17:59:11 +0300

    NewFeatures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+/*
+  Головний файл, в якому викликаються інші компоненти 
+*/
+
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import LOGIN from './components/LogIN/registr'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';    //використовуємо для маршрутизації 
+import Home from './components/Home/Home';
+import React from 'react';
+import createUser from './components/Registration/Registration';
+import Account from './components/Account/Account';
+import AdminMan from './components/Account/AdminPanel/AdminManegement';
+import UserList from './components/Account/AdminPanel/UserList';
+import EmployeeList from './components/Account/AdminPanel/EmployeeList';
+import SwipeComp from './components/SwipeComp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="main-container">
+            <Switch>                            {/* додаємо маршрути для компонент*/}
+              <Route exact path='/' component={Home} />
+              <Route path='/registration' component={createUser} />
+              <Route path='/account' component={Account}/>
+              <Route path='/admin' component={AdminMan}/>
+            </Switch>
+          </div>
+          <SwipeComp/>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
